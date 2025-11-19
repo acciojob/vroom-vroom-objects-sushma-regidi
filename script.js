e175458d17d72781ac05cd015432778222aa3f86
sushma-regidi <regidisushma@gmail.com>
2025-11-19 12:15:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 // Car constructor
 function Car(make, model) {
-  this.make = make;
-  this.model = model;
+    this.make = make;
+    this.model = model;
 }
 
+// Add method to Car prototype
 Car.prototype.getMakeModel = function () {
-  return this.make + " " + this.model;
+    return this.make + " " + this.model;
 };
 
 // SportsCar constructor
 function SportsCar(make, model, topSpeed) {
-  Car.call(this, make, model);
-  this.topSpeed = topSpeed;
+    // call Car constructor
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
-// Inherit from Car prototype
+// Inherit prototype from Car
 SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset constructor pointer
 SportsCar.prototype.constructor = SportsCar;
 
+// Add method to SportsCar prototype
 SportsCar.prototype.getTopSpeed = function () {
-  return this.topSpeed;
+    return this.topSpeed;
 };
 
-// ⭐ VERY IMPORTANT ⭐
-// Expose them to Cypress via window
-window.Car = Car;
-window.SportsCar = SportsCar;
 
 
 
