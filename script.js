2a1a84f40387f2c75cc255d004ca86c0507df484
sushma-regidi <regidisushma@gmail.com>
2026-01-06 16:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-// Car constructor function
-function Car(make, model) {
-    this.make = make;
-    this.model = model;
-}
 
-// Method on Car prototype
-Car.prototype.getMakeModel = function () {
-    return this.make + " " + this.model;
-};
-
-// SportsCar constructor function
-function SportsCar(make, model, topSpeed) {
-    Car.call(this, make, model);
-    this.topSpeed = topSpeed;
-}
-
-// Inherit from Car
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
-
-// Method on SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-    return this.topSpeed;
-};
-
-// 🔥 REQUIRED FOR CYPRESS TESTS 🔥
-window.Car = Car;
-window.SportsCar = SportsCar;
 
 
 
